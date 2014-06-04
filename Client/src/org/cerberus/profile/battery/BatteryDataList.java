8c9cb6341e82b043eff3ec4b6c0ed464f976de47
nohsunghyun <nohsunghyun@172.16.101.155>
2014-06-04 18:24:03 +0900

    fit:battery profiling

---
+++ Client/src/org/cerberus/profile/battery/BatteryDataList.java
@@ -0,0 +1,22 @@
+package org.cerberus.profile.battery;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class BatteryDataList {
+
+	private static List<Map> instance;
+	
+	
+	
+	public static List<Map> getInstance() {
+		
+		if(instance == null) {
+			instance = new ArrayList<Map>();
+		}
+		
+		return instance;
+	}
+	
+}
