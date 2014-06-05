fdc2c8ec27df5cc520094400da2deaca71788fd7
nohsunghyun <nohsunghyun@172.16.101.155>
2014-06-05 16:45:30 +0900

    fit:powertutor service interface

---
+++ Client/src/edu/umich/PowerTutor/service/UMLoggerService.java
@@ -0,0 +1,17 @@
+package edu.umich.PowerTutor.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+// interface class
+public class UMLoggerService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
