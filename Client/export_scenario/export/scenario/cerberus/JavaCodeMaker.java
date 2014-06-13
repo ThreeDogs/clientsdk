c66599e9c94693050d1c4284c8cdd822de1a5a23
nohsunghyun <nohsunghyun@172.16.101.126>
2014-06-13 23:33:27 +0900

    fit:code export asm

--- Client/export_scenario/export/scenario/cerberus/JavaCodeMaker.java
+++ Client/export_scenario/export/scenario/cerberus/JavaCodeMaker.java
@@ -13,6 +13,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
@@ -40,31 +41,40 @@ public class JavaCodeMaker {
 	 */
 	public static void main(String[] args) throws Exception {
 		
+		String mainActivity = args[0];
+//		String mainActivity = "com.example.test.MainActivity";
+		String packageName = args[1];
+//		String packageName = "com.example.test";
+		String scenarioKey = args[2];
+		String filePath = args[3];
 		
-//		String mainActivity = args[0];
-		String mainActivity = "com.example.test.MainActivity";
-//		String packageName = args[1];
-		String packageName = "com.example.test";
-//		String scenarioKey = args[2];
-//		String filePath = args[3];
+		
+		System.out.println(scenarioKey);
 		
 		//get scenario data
-//		String strJsonArr = getScenario(scenarioKey);
+		String strJsonArr = getScenario(scenarioKey);
 		
-		Scanner scanner = new Scanner( new FileInputStream( new File("scenario3.json") ));
-		StringBuilder temp=new StringBuilder();
-		while(scanner.hasNext()){
-			temp.append(scanner.next());
-		}
-		String strJsonArr = temp.toString();
+//		Scanner scanner = new Scanner( new FileInputStream( new File("scenario3.json") ));
+//		StringBuilder temp=new StringBuilder();
+//		while(scanner.hasNext()){
+//			temp.append(scanner.next());
+//		}
+//		String strJsonArr = temp.toString();
 
 //		System.out.println(strJsonArr);
+		
+		System.out.println(strJsonArr);
 		
 		//json parse
 		Gson gson = new Gson();
 		 JsonReader reader = new JsonReader( new StringReader(strJsonArr));
 	        reader.setLenient(true);
-		JsonArray scenarioJsonArr = gson.fromJson(reader, JsonArray.class);
+	        
+	    JsonObject jsonObj = gson.fromJson(reader, JsonObject.class);
+	        
+	    
+	    
+		JsonArray scenarioJsonArr = jsonObj.getAsJsonArray("motion_event_infos");
 		
 		
 		sb.append("import " + packageName + ".R;");
@@ -153,32 +163,35 @@ public class JavaCodeMaker {
 
 //				 sb.append("		Thread.sleep(2000);");
 				 sb.append("\n");
-				 sb.append("		Thread.sleep(" + (scenarioObjJson.get("sleep").getAsInt() + 2000) + ")");
+				 sb.append("		Thread.sleep(" + (scenarioObjJson.get("sleep").getAsInt() + 2000) + ");");
 				 sb.append("\n");
 			 }
-			
+			sb.append("\n");
+			sb.append("		// -------" + (i+1));
 			
 			if( scenarioObjJson.get("action_type").getAsString().equals("Click") ) {
 				sb.append("\n");
-				sb.append("		View clickView = solo.getView( R.id." + scenarioObjJson.get("view").getAsString() + ");");
+				sb.append("\n	{");
+				sb.append("			View clickView = solo.getView( R.id." + scenarioObjJson.get("view").getAsString() + ");");
 				sb.append("\n");
-				sb.append("		if(clickView instanceof Button) {");
+				sb.append("			if(clickView instanceof Button) {");
 				sb.append("\n");
-				sb.append("			solo.clickOnView( clickView );");
+				sb.append("				solo.clickOnView( clickView );");
 				sb.append("\n");
-				sb.append("		}");
+				sb.append("			}");
 				sb.append("\n");
-				sb.append("		else if(clickView instanceof TextView) {");
+				sb.append("			else if(clickView instanceof TextView) {");
 				sb.append("\n");
-				sb.append("			solo.clickOnText(((TextView)clickView).getText().toString());");
+				sb.append("				solo.clickOnText(((TextView)clickView).getText().toString());");
 				sb.append("\n");
-				sb.append("		}");
+				sb.append("			}");
 				sb.append("\n");
-				sb.append("		else {");
+				sb.append("			else {");
 				sb.append("\n");
-				sb.append("			solo.clickOnView( clickView );");
+				sb.append("				solo.clickOnView( clickView );");
 				sb.append("\n");
-				sb.append("		}");
+				sb.append("			}");
+				sb.append("\n	}");
 				sb.append("\n");
 			}
 			else if( scenarioObjJson.get("action_type").getAsString().equals("LongClick") ) {
@@ -194,7 +207,7 @@ public class JavaCodeMaker {
 				sb.append("\n");
 				sb.append("		solo.sleep(1500);");
 				sb.append("\n");
-				sb.append("		solo.clickOnText(\"설정\")");
+				sb.append("		solo.clickOnText(\"설정\");");
 				sb.append("\n");
 				
 			}
@@ -205,7 +218,7 @@ public class JavaCodeMaker {
 				sb.append("\n");
 				sb.append("		solo.setTimePicker( 0 , " + Integer.parseInt(param.split(",")[0]) + " , " + Integer.parseInt( param.split(",")[1]) + " );");
 				sb.append("\n");
-				sb.append("		solo.clickOnText(\"설정\")");
+				sb.append("		solo.clickOnText(\"설정\");");
 				sb.append("\n");
 				
 			}
@@ -250,7 +263,7 @@ public class JavaCodeMaker {
 				sb.append("		solo.clickOnView(listElement);");
 				sb.append("\n");
 			}
-			else if(scenarioObjJson.get("action_type").getAsString().equals("drag")) {
+			else if(scenarioObjJson.get("action_type").getAsString().equals("drag")) { 
 				
 				String param = scenarioObjJson.get("param").getAsString();
 				int x1 = Integer.parseInt( param.split(",")[0] );
@@ -283,6 +296,7 @@ public class JavaCodeMaker {
 				 String param = scenarioObjJson.get("param").getAsString();
 				 sb.append("\n");
 				 sb.append("		solo.clickOnText(\"" + param + "\");");
+				 
 				 sb.append("\n");
 			 }
 			
@@ -346,26 +360,27 @@ public class JavaCodeMaker {
 		
 		
 		sb.append("}");
-		System.out.println(sb.toString());
+//		System.out.println(sb.toString());
 		
-//		FileOutputStream fos = new FileOutputStream(new File(filePath + "/CerberusTestRunner.java"));
-//		fos.write(sb.toString().getBytes());
-//		fos.flush();
-//		fos.close();
+		FileOutputStream fos = new FileOutputStream(new File(filePath + "/CerberusTestRunner.java"));
+		fos.write(sb.toString().getBytes());
+		fos.flush();
+		fos.close();
 	}
 	
 	private static String getScenario(String scenarioKey) throws Exception {
-		String url = "/testSvr/ResultProfilingServlet" ;
+		String url = "http://localhost:3000/api/v1/test_scenarios/" + scenarioKey +"/motion_event_list" ;
+		System.out.println(url);
 		HttpClient client = new DefaultHttpClient();
-		ArrayList<NameValuePair> nameValuePairs = 
-				new ArrayList<NameValuePair>();
+//		ArrayList<NameValuePair> nameValuePairs = 
+//				new ArrayList<NameValuePair>();
 //		nameValuePairs.add(new BasicNameValuePair("data", gson.toJson(resultData)));
-		HttpPost httpPost = new HttpPost(url);
-		UrlEncodedFormEntity entityRequest = 
-				new UrlEncodedFormEntity(nameValuePairs, "EUC-KR");
-		httpPost.setEntity(entityRequest);
+		HttpGet httpGet = new HttpGet(url);
+//		UrlEncodedFormEntity entityRequest = 
+//				new UrlEncodedFormEntity(nameValuePairs, "EUC-KR");
+//		httpGet.setEntity(entityRequest);
 		
-		HttpResponse responsePost = client.execute(httpPost);
+		HttpResponse responsePost = client.execute(httpGet);
 		HttpEntity resEntity = responsePost.getEntity();
 //		System.out.println(EntityUtils.toString(resEntity)); 
 		String strJsonArr = EntityUtils.toString(resEntity);
